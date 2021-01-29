747694d379ec37ff40f920743e049ee901e3f624
Yasmin Jamshidi <34005423+Yasmojam@users.noreply.github.com>
2021-01-29 22:20:33 +0000

    player movement (#6)

---
+++ GGJ21/Assets/scripts/IsometricPlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricPlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed = 1f;
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPos = rigidBody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1); // prevent diagonal movement being faster
+        Vector2 movement = inputVector * movementSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        rigidBody.MovePosition(newPos);
+    }  
+
+
+}
