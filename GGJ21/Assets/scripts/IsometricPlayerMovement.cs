389deb061b15faadb329656fa5fd01a3c2f6754a
Darya Shumitskiy <darya.shumitskiy@skyscanner.net>
2021-01-30 19:53:21 +0000

    Fixed player distance to block bug

--- GGJ21/Assets/scripts/IsometricPlayerMovement.cs
+++ GGJ21/Assets/scripts/IsometricPlayerMovement.cs
@@ -34,30 +34,26 @@ public class IsometricPlayerMovement : MonoBehaviour
         inputVector = Vector2.ClampMagnitude(inputVector, 1); // prevent diagonal movement being faster
         Vector2 movement = inputVector * playerMovementSpeed;
         playerRigidBody.velocity = movement * Time.fixedDeltaTime;
-        canMove = Input.GetButton("Action");
-        Debug.Log("Horizontal input " + horizontalInput);
-        Debug.Log("Vertical input " + verticalInput);
-    }  
 
-    void OnCollisionEnter2D(Collision2D hit) {
-        if(hit.gameObject.tag == "BlockPuzzle") {
-            Debug.Log("Collision!");
-        }
+        canMove = Input.GetButton("Action");
     }
 
     void OnCollisionStay2D(Collision2D hit) {
         if(hit.gameObject.tag == "BlockPuzzle") {
+
             Vector2 currentPos = playerRigidBody.position;
             Vector2 hitCurrentPos = hit.rigidbody.position;
             float inputHorizontal = Input.GetAxis("Horizontal");
             float inputVertical = Input.GetAxis("Vertical");
             Vector2 distance = currentPos - hitCurrentPos;
-            Debug.Log("Can move " + canMove);
 
-            // 0.4, -0.2 right
-            // 0.4, 0.2 above
-            // -0.4, 0.2 left
-            // -0.4, -0.2 below
+            Debug.Log("Can move " + canMove);
+            Debug.Log("Distance " + distance);
+
+            // +x, -y right
+            // +x, +y above
+            // -x, +y left
+            // -x, -y below
 
             if(canMove) {
                 // Figure out if player is on left or right of stone
@@ -116,7 +112,6 @@ public class IsometricPlayerMovement : MonoBehaviour
     private void MoveBlock(BlockDirection direction, Collision2D hit) {
         Vector2 blockMovement;
         Vector2 blockInputVector = new Vector2(0, 0);
-        Debug.Log("Direction " + direction);
         switch(direction) {
             case BlockDirection.Up:
                 blockInputVector = new Vector2(2f, 1f);
