680308f2735ce89c71a8eac7ac9a6e65175a19e7
Iain Cattermole <iain_c@live.co.uk>
2021-01-31 02:08:49 +0000

    stone maybe work

--- GGJ21/Assets/scripts/BlockVelocity.cs
+++ GGJ21/Assets/scripts/BlockVelocity.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class BlockVelocity : MonoBehaviour
 {
+    int framesToMove = 75;  // 1.5secs
 
 	Rigidbody2D rigidbody;
-	bool movedThisFrame = false;
+
+    Vector2 targetDestination;
+    Vector2 startPosition;
+    int framesRemaining = 0;
+
+    bool moving = false;
 
     // Start is called before the first frame update
     void Start()
@@ -14,21 +20,46 @@ public class BlockVelocity : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void FixedUpdate() {
-    	if(!movedThisFrame) {
-    		rigidbody.velocity = new Vector2(0, 0);
-    	}
-    	movedThisFrame = false;
+        if (framesRemaining > 0) {
+            Vector2 newPosition = Vector2.MoveTowards(transform.position, targetDestination, 1f);
+
+            Vector2 diff = newPosition - (Vector2)transform.position;
+
+            if (diff.magnitude < 0.01) {
+                rigidbody.MovePosition(targetDestination);
+            } else {
+                rigidbody.velocity = diff * 3;
+            }
+            framesRemaining -= 1;
+        } else if (moving) {
+            Vector2 newPosition = Vector2.MoveTowards(transform.position, targetDestination, 10f);
+            Vector2 diff = newPosition - (Vector2)transform.position;
+
+            if (diff.magnitude < 0.05) {
+                rigidbody.MovePosition(targetDestination);
+            } else {
+                rigidbody.MovePosition(startPosition);
+            }
+
+            moving = false;
+        } else {
+            rigidbody.velocity = Vector2.zero;
+        }
     }
 
     public void Move(Vector2 blockMovement) {
-        rigidbody.velocity = blockMovement * Time.fixedDeltaTime;
-        movedThisFrame = true;
+        if (moving)
+            return;
+
+        startPosition = transform.position;
+        targetDestination = new Vector2(transform.position.x, transform.position.y) + blockMovement;
+
+        targetDestination = new Vector2(Mathf.Round(targetDestination.x * 2) / 2, Mathf.Round(targetDestination.y * 4) / 4);
+
+        Debug.Log(targetDestination);
+        moving = true;
+        framesRemaining = framesToMove;
     }
 }
