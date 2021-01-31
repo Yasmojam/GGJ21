d0c7dfbb9a832ef06bd33fd552dfc3a97843c264
Iain Cattermole <22656572+Iain530@users.noreply.github.com>
2021-01-31 11:39:20 +0000

    Bridge sorting order triggers (#32)

--- GGJ21/Assets/Scripts/UpdateZPosition.cs
+++ GGJ21/Assets/Scripts/UpdateZPosition.cs
@@ -7,21 +7,41 @@ public class UpdateZPosition : MonoBehaviour
     public Grid worldGrid;
     public float groundLevel = 1f;
 
+    float groundLevelOverride = 0f;
+
     public float topHalfZBoost = .75f;
     public float bottomHalfZBoost = 0f;
 
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, groundLevel + CalculateZBoost());
+        transform.position = new Vector3(transform.position.x, transform.position.y, groundLevel + groundLevelOverride + CalculateZBoost());
     }
 
 
     public float CalculateZBoost() {
-        Vector3 pos = new Vector3(transform.position.x, transform.position.y, groundLevel);
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y, groundLevel + groundLevelOverride);
         Vector3Int tilepos = worldGrid.WorldToCell(pos);
         Vector3 tileworldpos = worldGrid.CellToWorld(tilepos);
         Vector3 relativePos = pos - tileworldpos;
         //return relativePos.y + .25f;
         return relativePos.y < 0.25 ? bottomHalfZBoost : topHalfZBoost;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        OverrideGroundLevel overrideScript = collision.gameObject.GetComponent<OverrideGroundLevel>();
+        Debug.Log(collision.gameObject);
+
+        if (overrideScript) {
+            groundLevelOverride = overrideScript.additionalLevels;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        OverrideGroundLevel overrideScript = collision.gameObject.GetComponent<OverrideGroundLevel>();
+        Debug.Log(collision.gameObject);
+        // Make sure you left an override trigger
+        if (overrideScript) {
+            groundLevelOverride = 0;
+        }
     }
 }
