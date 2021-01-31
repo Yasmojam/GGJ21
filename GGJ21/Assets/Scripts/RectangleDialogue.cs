b4c739b6aeb52dd749482795266903d3d8b58adf
Dasha <darya_shum@live.co.uk>
2021-01-31 19:38:01 +0000

    Finish level 1 (#46)
    * Fix pause menu
    
    * bridge triggers
    
    * Level one now works - hooray!
    
    * Fix multiple rafts and missing barrier
    
    Co-authored-by: Iain Cattermole <iain_c@live.co.uk>
    Co-authored-by: yasmojam <yazzij97@gmail.com>
    Co-authored-by: Darya Shumitskiy <darya.shumitskiy@skyscanner.net>

--- GGJ21/Assets/Scripts/RectangleDialogue.cs
+++ GGJ21/Assets/Scripts/RectangleDialogue.cs
@@ -13,12 +13,12 @@ public class RectangleDialogue : MonoBehaviour {
     private List<string> dialogue = new List<string> {
         "Rectangle: Hey, you. You're finally awake. You were trying to cross the border, right? Our plane went down...",
         "Rectangle: Are they still arguing back there?",
-        "Rectangle: I can see some pine over there on the farthest islands...",
+        "Rectangle: I can see some dark wood over there on the farthest islands...",
         "Rectangle: I would go out and get them myself but my rectangular body is failing me...",
         "Rectangle: I did make some mattocks for you to collect materials and left them on each island...",
         "Rectangle: don't ask me how I got there...",
-        "Rectangle: If you complete each puzzle you get a mattock! You can use the wood and stones you pick up to navigate between islands to collect the pine",
-        "Rectangle: Rafts to get between sets of islands require 5 pine planks, bridges between islands require 2 planks and 2 stones. And the big boat needs 15 Pine planks and 10 stones",
+        "Rectangle: If you complete each puzzle you get a mattock! You can use the wood and stones you pick up to navigate between islands.",
+        "Rectangle: Rafts require 5 dark wood planks, bridges require 2 dark wood planks and 2 stones. And the big boat needs 15 dark wood planks and 10 stones",
 
     };
     private int dialogueIndex = 0;
